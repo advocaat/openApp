ecc0410957685ea44f55f2cf0b4d570595adf76f
advocaat <adam.nicholls@my.jcu.edu.au>
2016-04-06 15:58:32 +1000

    some interface and local database added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+  res.render('index', { pageName: 'Open Records', pageDescription: 'Login' });
 });
+
+
+
 
 module.exports = router;
