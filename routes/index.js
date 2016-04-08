e78ce29b03ae3ee9ac42b9af2c336f82431b9398
Graeme Broomhall <graeme.broomhall@my.jcu.edu.au>
2016-04-08 14:56:41 +1000

    added stats and piracy page

--- routes/index.js
+++ routes/index.js
@@ -13,5 +13,17 @@ router.get('/upload', function(req, res, next) {
   res.render('upload', { pageName: 'Open Records', pageDescription: 'Login' });
 });
 
+router.get('/stats', function(req, res, next) {
+
+
+  res.render('stats', { pageName: 'Open Records', pageDescription: 'Stats' });
+});
+
+router.get('/piracy', function(req, res, next) {
+
+
+  res.render('piracy', { pageName: 'Open Records', pageDescription: 'Piracy' });
+});
+
 
 module.exports = router;
